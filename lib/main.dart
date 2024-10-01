836d894cde200acadd153c573edf80fdb55a0632
Minh Nguyen <minh.nguyen@rikai.technology>
2024-10-01 17:08:01 +0700

    add new firebase CLI

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
